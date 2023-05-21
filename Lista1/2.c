fd7039aee93400664ca252c1ffae377bdbc39ef2
compermane <comper.mane@gmail.com>
2023-05-20 22:44:39 -0300

    Ajustes

--- Lista1/2.c
+++ Lista1/2.c
@@ -11,9 +11,14 @@ int main()
     // Lê o nome do arquivo
     scanf("%s", arq);
 
-    // Abre o arquivo para leitura
+    // Abre o arquivo para leitura e mostra uma mensagem de erro caso 
+    // o arquivo informado não exista
     f = fopen(arq, "r");
-
+    if(f == NULL)
+    {
+        printf("Não foi possível encontrar o arquivo '%s'\n", arq);
+        return 0;
+    }
     // Lê todos os caracteres do arquivo
     while(1)
     {
